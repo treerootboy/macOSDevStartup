5d2caeaabc5986a9aad96fb509d5c7aeffdd526d
TR <treerootboy@gmail.com>
2019-04-13 14:37:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 curl -sL https://github.com/treerootboy/macOSDevStartup/archive/master.zip -o /tmp/macOSDevStartup.zip
 unzip -o /tmp/macOSDevStartup.zip -d /tmp/
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle
 cat /tmp/macOSDevStartup-master/npmfile | xargs -I{} npm i -g {}
 cat /tmp/macOSDevStartup-master/composer | xargs -I{} composer require -g {}
 rm -rf /tmp/macOSDevStartup-master
