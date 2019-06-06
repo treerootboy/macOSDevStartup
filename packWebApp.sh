bf50d9ab1cd04f0526c18ce5708a2a2f3d1f030e
TR <treerootboy@gmail.com>
2019-06-06 14:28:51 +0800

    Update packWebApp.sh

--- packWebApp.sh
+++ packWebApp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir -p webapp
+cd webapp
 nativefier --name "stackedit" "https://stackedit.io/app"
 nativefier --internal-urls ".*?\.(tapd)\.*?" --name "TAPD" "https://www.tapd.cn"
 nativefier --name Gmail --internal-urls ".*?\.(google.com|gmail.com)\.*?" https://gmail.com
+mv */*.app /Applications
