a522dd07bef15f881f584e1d4be237af3b54d83d
TR <treerootboy@gmail.com>
2019-06-06 14:09:42 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# 备份 npm 包
 npm ls  -g --depth=0  2>&1 | grep "──" | awk '{print $2}' > npmfile
+
+# 备份 composer 包
 \cp -f ~/.composer/composer.json composer.json
+
+# 备份 brew 包
+brew bundle dump
 
