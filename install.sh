9c92203fd0c585acbf7ee43573821f76f6fad3ee
TR <treerootboy@gmail.com>
2023-10-06 08:26:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 curl -sL https://github.com/treerootboy/macOSDevStartup/archive/master.zip -o /tmp/macOSDevStartup.zip
 unzip -o /tmp/macOSDevStartup.zip -d /tmp/
 cd /tmp/macOSDevStartup-master/
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew bundle --file=Brewfile
 cat npmfile | xargs -I{} npm i -g {}
 cp composer.json ~/.composer/composer.json
